7c0f026e75790d10bf977b957492a7126c766b2b
tomrod10 <tomasrod09@hotmail.com>
2025-06-07 21:57:22 -0700

    chapter 3 exercises

---
+++ Programming in C/Chapter 3/exercise_2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("In C, lowercase letters are important.\n");
+    printf("main is where program execution begins.\n");
+    printf("Opening and closing braces enclose program statements in a routine.\n");
+    printf("All program statements must be terminated by a semicolon.\n");
+
+    return 0;
+};
---
+++ Programming in C/Chapter 3/exercise_4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 87, b = 15, res;
+
+    res = a - b;
+    printf("%i minus %i is equal to %i\n", a, b, res);
+
+    return 0;
+};
