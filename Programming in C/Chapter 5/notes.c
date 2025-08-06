dd6b3dc741da7d930714627cd7c56dd8d35ad6b1
tomrod10 <tomasrod09@hotmail.com>
2025-08-05 23:28:17 -0700

    progress

--- Programming in C/Chapter 5/notes.c
+++ Programming in C/Chapter 5/notes.c
@@ -2,9 +2,31 @@
 
 /*
 [The for Statement]
+
+Relation Operators (i.e > or <=) have lower precedence
+compared to Arithmetic Operators
 */
+
+// Printing without column width
+int print_col_width()
+{
+    int i = 10, j = 20, k = 5;
+    printf(" i      Range\n");
+    printf("---     -----\n");
+    printf("%i         %i\n", k, j);
+    printf("%i         %i\n", i, j); // won't be centered properly
+
+    printf("\n");
+    printf(" i      Range\n");
+    printf("---     -----\n");
+    printf("%2i        %i\n", k, j); // centered with column width
+    printf("%2i        %i\n", i, j);
+
+    return 0;
+};
 
 int main()
 {
+    print_col_width();
     return 0;
-}
+};
